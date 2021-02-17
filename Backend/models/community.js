562b887c4e03889aa836340060894f0dc9b0bd45
Gaurav Singh <gauravkorlasingh@gmail.com>
2021-02-17 18:14:40 +0530

    Added website field to community model

--- Backend/models/community.js
+++ Backend/models/community.js
@@ -9,7 +9,8 @@ var CommunitySchema = new Schema({
     admin: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     description: { type: String },
     communitySkills: [ {type: Schema.Types.ObjectId, ref: 'Skill' }],
-    displayPicture: {type: String, default: null}
+    displayPicture: {type: String, default: null},
+    website: {type: String, default: null}
 })
 
 CommunitySchema.statics.findCommunityByShortName = function findCommunityByShortName(shortName, callback) {
