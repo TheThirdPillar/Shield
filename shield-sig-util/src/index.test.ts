56b9e9981a3d3fc861d2add12f8fb98e55dcd4ac
Gaurav Singh <gauravkorlasingh@gmail.com>
2022-06-24 21:08:37 +0530

    Updated the shield sig util

---
+++ shield-sig-util/src/index.test.ts
@@ -0,0 +1,34 @@
+import { concatSig, normalize } from './utils';
+import * as sigUtil from '.';
+
+describe('exports', () => {
+  it('should have all expected exports', () => {
+    expect(Object.keys(sigUtil)).toMatchInlineSnapshot(`
+Array [
+  "concatSig",
+  "normalize",
+  "personalSign",
+  "recoverPersonalSignature",
+  "extractPublicKey",
+  "SignTypedDataVersion",
+  "TYPED_MESSAGE_SCHEMA",
+  "TypedDataUtils",
+  "typedSignatureHash",
+  "signTypedData",
+  "recoverTypedSignature",
+  "encrypt",
+  "encryptSafely",
+  "decrypt",
+  "decryptSafely",
+  "getEncryptionPublicKey",
+]
+`);
+  });
+
+  // I don't know why this is necessary.
+  // I tried using an 'istanbul ignore next' comment but it did not work.
+  it('should stop marking these functions as not covered', () => {
+    expect(sigUtil.concatSig).toBe(concatSig);
+    expect(sigUtil.normalize).toBe(normalize);
+  });
+});
